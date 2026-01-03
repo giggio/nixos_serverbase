e090cc5817c5cf6ef79459ba1fec55a836cc92bb
Giovanni Bassi <giggio@giggio.net>
2026-01-03 19:34:42 -0300

    Remove commented code in vm.nix

--- vm.nix
+++ vm.nix
@@ -5,26 +5,6 @@
     "${toString modulesPath}/virtualisation/virtualbox-image.nix"
   ];
 
-  # fileSystems = {
-  #   "/" = {
-  #     device = "/dev/disk/by-label/nixos";
-  #     fsType = "ext4";
-  #   };
-  # };
-  # fileSystems = {
-  # "/" = {
-  #   device = "/dev/sda2";
-  #   fsType = "ext4";
-  # };
-
-  # "/boot" = {
-  #   # device = "/dev/sda1";
-  #   device = "/dev/disk/by-label/boot";
-  #   fsType = "vfat";
-  #   # options = [ "fmask=0022" "dmask=0022" ];
-  # };
-  # };
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   virtualisation = {
