79b6eba3712f5dd7a236f43b03704ad7df7f6362
Giovanni Bassi <giggio@giggio.net>
2026-07-15 19:58:36 -0300

    Mock virtualisation.fileSystems
    This is required because importing the whole `qemu-vm.nix` module will cause several
    side effects.

--- modules/config-physical.nix
+++ modules/config-physical.nix
@@ -1,2 +1,13 @@
 {
+  options,
+  lib,
+  config,
+  ...
+}:
+
+{
+  # the guard is required only because vmboot also imports this module
+  options.virtualisation = lib.attrsets.optionalAttrs (!config.setup.isVM) {
+    fileSystems = options.fileSystems;
+  };
 }
