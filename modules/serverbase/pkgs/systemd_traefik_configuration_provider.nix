99dcf245de072ced4a901e0df842401c45d73c87
Giovanni Bassi <giggio@giggio.net>
2026-07-20 00:31:41 -0300

    Update systemd_traefik_configuration_provider

--- modules/serverbase/pkgs/systemd_traefik_configuration_provider.nix
+++ modules/serverbase/pkgs/systemd_traefik_configuration_provider.nix
@@ -11,12 +11,12 @@
 }).buildRustPackage
   rec {
     pname = "systemd_traefik_configuration_provider";
-    version = "0.1.0";
+    version = "0.1.1";
 
     src = fetchgit {
       url = "https://codeberg.org/giggio/systemd_traefik_configuration_provider.git";
-      rev = "cef3f9873bcb73bfc979773a0e9e3420da83d529";
-      hash = "sha256-xLfPOWu6wnHfYuGhQcml2Fd4LG89ED5jkn20cgooq5M=";
+      rev = "a41445127280dc4983cd2ae3edf65394f1b914c2";
+      hash = "sha256-9N4Z7HnVEUW0d/YIf0FXIqJd6dx79JrCiMZb+fOb6ps=";
     };
 
     cargoLock.lockFile = "${src}/Cargo.lock";
